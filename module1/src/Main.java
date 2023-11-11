c03992f1e204e650a32be63aee574345ff29d7dd
ManHingHUI <xuwenxing00@gmail.com>
2023-11-11 19:35:46 +0800

    first commit

---
+++ module1/src/Main.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+    }
+}
+//cmd + o   搜索類查看文檔
+//cmd + f12 查看當前文檔的結構qqqqqq
+
+//                      soft-wrap       => opt + z
+//cmd + opt + l         reformat code   => opt + shift + f
+//cmd + shift + enter   complete current statement
+
